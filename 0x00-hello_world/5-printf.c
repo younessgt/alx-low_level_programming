1e9cc5bc983cf7a47524c61502882f40c606f30e
younessgt <youness.merzak123@gmail>
2023-03-16 10:30:22 -0700

    5-printf.c script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Entry point,printing message
+ * Return: 0
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
