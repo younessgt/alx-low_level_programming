c5c7f08cb462ade7496c8a713a239628e7315889
younessgt <youness.merzak123@gmail>
2023-03-16 11:01:33 -0700

    6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 int main (void)
 {
 
-printf("the size of chart %d byte(s)\n",sizeof(char));
-printf("the size of chart %d byte(s)\n",sizeof(int));
-printf("the size of chart %d byte(s)\n",sizeof(long int));
-printf("the size of chart %d byte(s)\n",sizeof(long long int));
-printf("the size of chart %d byte(s)\n",sizeof(float));
+printf("the size of a chart: %d byte(s)\n",sizeof(char));
+printf("the size of an int: %d byte(s)\n",sizeof(int));
+printf("the size of a long int: %d byte(s)\n",sizeof(long int));
+printf("the size of a long long int: %d byte(s)\n",sizeof(long long int));
+printf("the size of a float: %d byte(s)\n",sizeof(float));
 return (0);
 }
 
