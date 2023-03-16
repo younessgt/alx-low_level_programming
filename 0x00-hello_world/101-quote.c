ab8d6bf5d57cb302e3d338240282e3114a559744
younessgt <youness.merzak123@gmail>
2023-03-16 16:11:14 -0700

    101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,15 @@
  */
 int main(void)
 {
-	char str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(str);
-return (0);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (int i=0; i < max; i++)
+	{
+	if (str[i] == '\0')
+	{
+	putchar('\n');
+	break;
+	}
+	else putchar(str[i]);
+	}
+return (1);
 }
