be0530eea00de9c5ba0a89964460085a645180ff
younessgt <youness.merzak123@gmail>
2023-03-16 14:47:48 -0700

    101-quote.c script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ * Return: 1
+ */
+int main(void)
+{
+	char str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+	putchar(str);
+return (1);
+}
