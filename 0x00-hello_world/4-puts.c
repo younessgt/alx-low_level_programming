f2e6d60a1d1dba43e3b842d6eadf717a65adae01
younessgt <youness.merzak123@gmail>
2023-03-16 08:55:56 -0700

    4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 int main() {
 
           /* initialisation string */
-   char str[] = "\"Programming is like building a multilingual puzzle ";
+   char str[] = "\"Programming is like building a multilingual puzzle";
           /* printing str */
    puts(str);   
  return (0);
