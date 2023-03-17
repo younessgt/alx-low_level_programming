385ae5012527f456425a58b845bf4c23365e0aa5
younessgt <youness.merzak123@gmail>
2023-03-17 16:35:41 -0700

    101-print_comb4 C program

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+        int a;
+        int b;
+	int c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			for (c = b +1; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 8)
+				{
+					 putchar(',');
+					 putchar(' ');
+				}
+			}			
+		}
+	}
+putchar('\n');
+return (0);
+}
