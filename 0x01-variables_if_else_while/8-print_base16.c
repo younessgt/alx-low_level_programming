e48c7d8581e517e26c9797c8e93b641f5c2422e5
younessgt <youness.merzak123@gmail>
2023-03-17 11:37:47 -0700

    8-print_base16 C program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Retur: 0
+ */
+int main(void)
+{
+	int i;
+	int num = 18;
+	char Str[] = "0123456789abcdef";
+
+	for (i = 0; i < num; i++)
+	{
+		if (Str[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(Str[i]);
+	}
+return (0);
+}
