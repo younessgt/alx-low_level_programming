d5fa3e384c909934f479e0a54bda25503ac24b08
younessgt <youness.merzak123@gmail>
2023-03-17 10:28:00 -0700

    7-print_tebahpla C program

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print alphabets in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        char Alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+        for (i = 26; i >= 0; i--)
+        {
+		 putchar(Alpha[i]);
+        }
+
+	putchar('\n');
+return (0);
+}
