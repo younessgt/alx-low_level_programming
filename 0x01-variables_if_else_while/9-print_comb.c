de275d35f7261f06dceef06dc42099431be73af8
younessgt <youness.merzak123@gmail>
2023-03-17 12:39:26 -0700

    9-print_comb C program

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,7 @@ int main(void)
 {
         int i;
         int N = 10;
-        char Str[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int Str[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for (i = 0; i < N; i++)
         {
