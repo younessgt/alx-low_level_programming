575abe3c2b2eee6a75f1f8f097408c46354abaf7
younessgt <youness.merzak123@gmail>
2023-03-17 15:02:11 -0700

    100-print_comb3 C program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			
+			if (a != 8)
+			{
+				putchar(',');
+				putchar(' ');	
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
