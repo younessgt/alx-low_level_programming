fac16a18539427a4e882d0b171af0c52b85fe60b
younessgt <youness.merzak123@gmail>
2023-03-17 06:17:20 -0700

    1-last_digit.c script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
 
-	if (n != 0)
+	if (ld != 0)
 	{
-		if (n > 5)
+		if (ld > 5)
 		{
 			printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 		}
-		if (n < 6)
+		if (ld < 6)
 		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 		}
 	}
-	
-	if (n == 0)
+	if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	
