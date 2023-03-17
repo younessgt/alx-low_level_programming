ebca8558f0083ee30a209b133c6e130fc84625cd
younessgt <youness.merzak123@gmail>
2023-03-17 09:51:09 -0700

    6-print_numberz C program

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,23 +5,15 @@
  */
 int main(void)
 {
-	int size = 12;
+	int size = 10;
 	int i;
-	int tabl[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int num = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		if (tabl[i] == '\0')
-		{
-			putchar('\n');
-			break;
-		}
-		else
-		{	
-			putchar(tabl[i] + '0');
-
-		}
-
+		putchar(num + '0');
+		num++;
 	}
+	putchar('\n');
 return (0);	
 }
