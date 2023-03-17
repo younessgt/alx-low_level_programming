4bf069fd22b7191f740ec409c345299712d35163
younessgt <youness.merzak123@gmail>
2023-03-17 16:39:04 -0700

    101-print_comb4 C program

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,25 +5,25 @@
  */
 int main(void)
 {
-        int a;
-        int b;
+	int a;
+	int b;
 	int c;
 	
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = a + 1; b <= 9; b++)
 		{
-			for (c = b +1; c <= 9; c++)
+			for (c = b + 1; c <= 9; c++)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');
-				if (a != 8)
+				if (a != 7)
 				{
 					 putchar(',');
 					 putchar(' ');
 				}
-			}			
+			}
 		}
 	}
 putchar('\n');
