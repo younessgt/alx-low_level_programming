63ea8be34a6a27d619a090fc4d712d811513e8d0
younessgt <youness.merzak123@gmail>
2023-03-17 18:01:45 -0700

    102-print_comb5 C program

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 8; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+
+					if (a != 9 || b != 8 || c != 9 || d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					
+					}
+				
+				}
+			
+			}
+		
+		
+		}
+	
+	}
+putchar('\n');
+return (0);
+}
