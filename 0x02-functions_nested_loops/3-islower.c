3939104d2792583b460fa21e24c5ed5da37c20d1
younessgt <youness.merzak123@gmail>
2023-03-21 10:42:07 -0700

    3-islower C program

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,10 @@
  */
 int _islower(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (0);
-	else if (c == 0)
-		return (0);
-	else
+	if (c >= 'a' && c <= 'z')
 		return (1);
+	/*else if (c == 0)
+		return (0);*/
+	else
+		return (0);
 }
