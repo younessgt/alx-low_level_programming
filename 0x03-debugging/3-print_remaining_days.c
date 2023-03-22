00ea20dea105de2b5ac4ccaebea4cee7b7196c8f
younessgt <youness.merzak123@gmail>
2023-03-22 11:37:12 -0700

    Fourth Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,10 +18,6 @@ void print_remaining_days(int month, int day, int year)
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-		else if (month == 2 && day == 60)
-		{
-
-		}
 	}
 	else if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 	{
