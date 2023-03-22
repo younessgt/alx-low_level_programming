97a11770f38165a0847f09c6a8ea565c13b0ae49
younessgt <youness.merzak123@gmail>
2023-03-22 16:23:31 -0700

    nat

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1023; i >= 0; i--)
+	{
+		if (i % 3 == 0)
+			sum = sum + i;
+		else if (i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n",sum);
+return (0);
+}
