77871442d4d4425f529df94fd0b4a35c2a71f99a
younessgt <youness.merzak123@gmail>
2023-03-22 06:03:56 -0700

    Second Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	
 	i = 0;
 	
-	while (i < 10)
+	/*while (i < 10)
 	{
-		putchar(i);/* Not incrementing 'i' is causing the problem*/
-	}
+		putchar(i);
+	}*/
 	
 	printf("Infinite loop avoided! \\o/\n");
 	
