f01633d53fcaecf81d44e039f64fd5592ff7871f
younessgt <youness.merzak123@gmail>
2023-03-21 17:27:06 -0700

    10-add C program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @a: first  variables to enter
+ * @b: second number to enter
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	n = add(89, 9);
+	printf("%d\n", n);
+
+return (0);
+}
