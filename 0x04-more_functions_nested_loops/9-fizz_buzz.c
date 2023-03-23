dcd1587cfa48eb99ec9de57a5abce8abae420529
younessgt <youness.merzak123@gmail>
2023-03-23 11:13:28 -0700

    fizz_buzz C program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,17 @@ int main(void)
 		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
+
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 return (0);
