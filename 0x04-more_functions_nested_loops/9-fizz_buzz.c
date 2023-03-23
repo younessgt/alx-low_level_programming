18c5ff7160b81dcd01f5f532b2d1908de9e44c74
younessgt <youness.merzak123@gmail>
2023-03-23 10:40:28 -0700

    fizz_buzz C program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz Function
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d", i);
+	}
+return (0);
+}
