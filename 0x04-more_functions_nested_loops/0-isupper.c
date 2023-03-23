9443a100b5551fe66e70e71661b9df422d48b90c
younessgt <youness.merzak123@gmail>
2023-03-23 05:18:35 -0700

    icupper C fonction

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _isupper - function that checks for uppercase character
+ *  @c: The character that we will check
+ *  Return:1 if true 0 if flase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar('1' + '0');
+		return (1);
+	}
+	else
+	{
+		_putchar('0' + '0');
+		return (0);
+	}
+}
