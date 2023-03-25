640317d95fafe34d6275e3fe18998b7b718d394c
younessgt <youness.merzak123@gmail>
2023-03-25 15:10:53 -0700

    103-fibonacci C program

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program that finds and prints the sum of the even-valued terms
+ * Return: 0 always
+ */
+int main(void)
+{
+	long i = 0;
+	long j = 1;
+	long sum1;
+	long sum2 = 0;
+
+	while (sum1 < 4000000)
+	{
+		sum1 = i + j;
+		i = j;
+		j = sum1;
+		if (sum1 % 2 == 0)
+			sum2 = sum2 + sum1;
+	}
+	printf("%lu", sum2)
+return (0);
+}
