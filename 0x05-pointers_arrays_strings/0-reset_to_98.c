45ac2ebb2f0abd4ae748daab69dada2002a1081a
younessgt <youness.merzak123@gmail>
2023-03-27 06:14:18 -0700

    reset_to_98 C function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - reseting the value of an integer
+ * @n: The pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is for Pointers, arrays and strings
