76950139c9a0d2e24dda982a6c006afebf9ba6d5
younessgt <youness.merzak123@gmail>
2023-04-05 07:46:05 -0700

    0-puts_recursion C function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(s[i]);
-	i++;
 	if (s[i] == '\0')
 		_putchar('\n');
 	else
+	{
+		_putchar(s[i]);
+		i++;
 		_puts_recursion(s + i);
+	}
 }
