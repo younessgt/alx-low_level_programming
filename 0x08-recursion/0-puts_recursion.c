e63eb623268cac0311c5633fa02cebf31ef3f7c4
younessgt <youness.merzak123@gmail>
2023-04-05 07:31:41 -0700

    0-puts_recursion C function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ * @s: pointer to character
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	_putchar(s[i]);
+	i++;
+	if (s[i] == '\0')
+		_putchar('\n');
+	else
+		_puts_recursion(s + i);
+}
