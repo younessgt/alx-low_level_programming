5491d87a856d8d829d75bdd51a0392a3bd7ffcb1
younessgt <youness.merzak123@gmail>
2023-04-08 07:10:15 -0700

    0-whatsmyname program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * main - entry point, printing the program's name
  * @argc: integer count number of arguments
  * @argv: argument vector
- * Return : 0 (Success)
+ * Return: 0 (Success)
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc-1]);
 	return (0);
 }
