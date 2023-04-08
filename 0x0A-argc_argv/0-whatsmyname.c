de73d2bef04e8137c13b37108c78f9fad6756151
younessgt <youness.merzak123@gmail>
2023-04-08 07:02:03 -0700

    0-whatsmyname program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point, printing the program's name
+ * @argc: integer count number of arguments
+ * @argv: argument vector
+ * Return : 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
