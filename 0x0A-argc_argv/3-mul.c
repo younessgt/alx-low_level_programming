cf7843a48124b35f64bb9636d609881fc015eb41
younessgt <youness.merzak123@gmail>
2023-04-08 08:49:50 -0700

    3-mul program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int Mu = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			Mu = Mu * atoi(argv[i]);
+		}
+	}
+	printf("%d\n", Mu);
+	return (0);
+}
