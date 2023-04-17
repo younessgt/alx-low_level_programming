9c0f22cffe3699c2fc473aac53765b862832a56d
younessgt <youness.merzak123@gmail>
2023-04-17 06:22:18 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,6 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age < 0)
-		d->age = 0;
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name : %s\n", d->name);
