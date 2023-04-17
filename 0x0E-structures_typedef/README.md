d1d1924bc2be95e46d1d0404c54a2f9c9adf8602
younessgt <youness.merzak123@gmail>
2023-04-17 05:18:19 -0700

    dog.h header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory is for structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure containing dog informations
+ * @name: dog name
+ * @age: the age
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
