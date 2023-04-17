a2b7d2204ec592c13d785c863b13b232b1f07737
younessgt <youness.merzak123@gmail>
2023-04-17 12:46:52 -0700

    2-main c program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point , printing the name of the file
+ * using predefined macros
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
