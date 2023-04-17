09045abd7b13a8eca02617d1d3cb781918f17f95
younessgt <youness.merzak123@gmail>
2023-04-17 06:19:58 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -10,7 +10,8 @@ int main(void)
 {
     struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    my_dog.age = -2;
+
+    print_dog(&my_dog);
     return (0);
 }
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - function that prints a struct dog
+ * @d: pointer to structure
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->age < 0)
+		d->age = 0;
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name : %s\n", d->name);
+	printf("Age : %f\n", d->age);
+	printf("Owner : %s\n", d->owner);
+
+}
