e655ba0f9527d35df2bf83378b681dff6eb94454
younessgt <youness.merzak123@gmail>
2023-04-20 13:22:43 -0700

    3-print_all c function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,11 +34,9 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-			default:
-				i++;
-				continue;
 		}
-		if (format[i + 1] != '\0')
+		if (format[i + 1] != '\0' && (format[i + 1] == 'c' || format[i + 1] == 'i'
+					|| format[i + 1] == 'f' || format[i + 1] == 's'))
 			printf(", ");
 		i++;
 
