a770e507e6813e4ff455286e54aae367ba1fc0a8
younessgt <youness.merzak123@gmail>
2023-04-20 05:25:02 -0700

    1-print_numbers c function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 		if (i < n - 1 && separator != NULL)
-			printf("%c", *separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
