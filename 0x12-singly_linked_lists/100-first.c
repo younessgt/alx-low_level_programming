f233569ab85cb8b17f4dc62873bdacbff0807f70
younessgt <youness.merzak123@gmail>
2023-04-28 16:47:35 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
  * exec_before_main - fonction that print before main
  * Return: nothing
  */
-void exec_before_main() __attribute__((constructor));
-void exec_before_main()
+void exec_before_main(void) __attribute__((constructor));
+void exec_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
