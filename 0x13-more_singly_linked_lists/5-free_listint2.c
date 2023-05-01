7121401731d62d129e49a22fca6afb0621cea53e
younessgt <youness.merzak123@gmail>
2023-05-01 05:13:21 -0700

    5-free_listint2 c function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 -  function that frees a list_t
+ * @head: pointer to pointer
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *stock;
+	listint_t *free_list;
+
+	stock = *head;
+	while (stock != NULL)
+	{
+		free_list = stock;
+		stock = stock->next;
+		free(free_list);
+	}
+	*head = NULL;
+}
