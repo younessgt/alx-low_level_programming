f026102e0e6fea7b6dd369255c079d2e3d54cf1d
younessgt <youness.merzak123@gmail>
2023-05-01 12:04:35 -0700

    9-insert_nodeint c function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = add;
 		return (*head);
 	}
-	for (i = 0; i < idx - 1 && (temp1 != NULL); i++)
+	for (i = 0; (i < (idx - 1)) && (temp1 != NULL); i++)
 		temp1 = temp1->next;
 	if (temp1 == NULL || temp1->next == NULL)
 	{
