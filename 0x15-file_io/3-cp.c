d145d0bf028d7178aa7a3c92bcc30f226528aa35
younessgt <youness.merzak123@gmail>
2023-05-08 16:36:31 -0700

    3-cp c function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
  */
 void close_file(int f)
 {
-	dprintf(2, "Error: Can't close fd %d\n", f);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
 	exit(100);
 }
 /**
@@ -20,10 +20,10 @@ void file_err(char *file, int i)
 	switch (i)
 	{
 		case 98:
-			dprintf(2, "Error: Can't read from file %s\n", file);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 			exit(98);
 		case 99:
-			dprintf(2, "Error: Can't write to %s\n", file);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 			exit(99);
 	}
 }
@@ -38,17 +38,17 @@ int main(int argc, char **argv)
 	int fo1, fo2;
 	ssize_t fr, fw;
 	char stock[1024];
-/*	mode_t UGO = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);*/
+	mode_t UGO = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fo1 = open(argv[1], O_RDONLY);
 	if (fo1 == -1)
 		file_err(argv[1], 98);
-	fo2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fo2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, UGO);
 	if (fo2 == -1)
 		file_err(argv[2], 99);
 	while ((fr = read(fo1, stock, 1024)) != 0)
