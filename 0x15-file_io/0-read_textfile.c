05b580da71f098fc7ec870cf2bce81302ace0ee4
younessgt <youness.merzak123@gmail>
2023-05-08 08:26:21 -0700

    0-read_textfile c function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it to
+ * the POSIX standard output
+ * @filename: pointer to character "file to read from"
+ * @letters: number of letters that should be read and printed
+ * Return: 0 if the file doesn't exist, can't be opened or the
+ * writing process fails
+ * else the number of bytes read is returned
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *stock;
+	int fo;
+	ssize_t fr, fw;
+
+	if (filename == NULL)
+		return (0);
+	stock = malloc(sizeof(char) * letters);
+	if (stock == NULL)
+		return (0);
+	fo = open(filename, O_RDONLY);
+	fr = read(fo, stock, letters);
+	fw = write(STDOUT_FILENO, stock, letters);
+	if (fo == -1 || fr == -1 || fw == -1)
+	{
+		free(stock);
+		return (0);
+	}
+	free(stock);
+	close(fo);
+	return (fr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory is for File I/0
