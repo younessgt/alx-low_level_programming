afd6cdae2f4142eb9503c62990e083aefd702fe5
yonessgt <youness.merzak123@gmail.com>
2023-06-15 12:13:15 +0000

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * dlistint_len - function  that returns the number of elements 
+ * dlistint_len - function  that returns the number of elements
  * in a struct list
  * @h: pointer to structure
  * Return: number of nodes
