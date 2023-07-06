d8aa9a2fd5de575b355f154d3517c1702fa92bc8
younessgt <youness.merzak123@gmail.com>
2023-07-06 17:23:52 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,16 +16,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (table == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, table->size);
-/*	printf("index is %lu\n", index);*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-/*	printf("new_node is %p\n", (void *)new_node);*/
 	new_node->key = strdup(key);
-/*	printf("key is %s\n", new_node->key);*/
 	new_node->value = strdup(value);
-/*	printf("value is %s\n", new_node->value);*/
-/*	printf("table index is %p\n", (void *)table->array[index]);*/
 	if (table->array[index] == 0)
 		new_node->next = NULL;
 	else
