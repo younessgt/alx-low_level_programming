dc85fcf10107f2fc319197562580bdd60cbcb0d2
younessgt <youness.merzak123@gmail.com>
2023-07-06 17:36:43 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node);
-		return (0)
+		return (0);
 	}
 	if (table->array[index] == 0)
 		new_node->next = NULL;
